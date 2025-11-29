e3264ac25c0d5fd0e7ae720043727de94fbdaa93
andreasaporito <andrea.saporito03@gmail.com>
2025-11-29 16:52:29 +0100

    Coded actual Printers, miss conceptual fixes

--- ROOT/ROOT/writer.cpp
+++ ROOT/ROOT/writer.cpp
@@ -4,20 +4,29 @@
 #include <memory>
 #include <string>
 
-Writer::Writer(Eigen::Matrix2d& vals_to_write) {
+Writer::Writer(const Eigen::MatrixX2d& vals_to_write) {
     values = vals_to_write;
 }
 
-void Writer::print_final_result() {
+void Writer::print_final_result() const {
     std::cout << "The found root is" << values.row(values.rows() - 1)(0) << std::endl;
 }
-
+// Add the possibility to either overwrite the file or to flush it
 void Writer::writing_process() {
     print_final_result();
+
     std::unique_ptr<Printer> printer;
     choose_how(printer);
-    for (auto row : values.rowwise()) {
+
+    for (int i = 0; i < values.rows(); i++) {
+        Eigen::Vector2d row = values.row(i);
         printer->write_values(row);
+    }
+
+    // LLM
+    if (auto gp = dynamic_cast<GnuplotPrinter*>(printer.get())) {
+        gp->generate_gnuplot_script();
+        std::cout << "Gnuplot script generated: plot.plt\n";
     }
 }
 
@@ -26,45 +35,49 @@ void Writer::choose_how(std::unique_ptr<Printer>& printer) {
     std::cout << "Do you want to produce a plot or to print the results somewhere?" << std::endl;
     std::cin >> choice1;
     if (choice1 == 0) {
-        printer = std::make_unique<VisualPrinter>();
+        printer = std::make_unique<OutputPrinter>();
     }
 }
+// I chose to let the Printer open the file each time it is called bc its job is just to handle the printing
+// Could be possible to let it print some iterations somewhere and others elsewhere
 
-VisualPrinter::VisualPrinter() {
-    std::cout << "Insert the filename of the file" << std::endl;
-    std::cin >> filename;
-}
-
-void VisualPrinter::write_values(Eigen::Vector2d value) {
-    std::cout << value(0) << "   " << value(1) << std::endl;
-}
+// maybe ask file names and stuff in Writer and pass as input in the consttructors
 
 OutputPrinter::OutputPrinter() {
     std::cout << "Here are the iterations of the method";
 }
 
-void OutputPrinter::write_values(Eigen::Vector2d value) {
+void OutputPrinter::write_values(const Eigen::Vector2d& value) {
     std::cout << "x = " << value(0) << "--- f(x) = " << value(1) << std::endl;
 }
 
 FilePrinter::FilePrinter() {
     std::cout << "Insert the filename of the file, without the extension" << std::endl;
+    std::cin >> filename;
 }
 
-csvPrinter::csvPrinter() {
+csvPrinter::csvPrinter() : FilePrinter() {
     filename.append(".csv");
-    std::cout << "Insert the separator" << std::endl;
+    std::cout << "Insert the separator: " << std::endl;
     std::cin >> separator;
 }
 
-void csvPrinter::write_values(Eigen::Vector2d value) {
-    std::cout << "x = " << value(0) << "--- f(x) = " << value(1) << std::endl;
+void csvPrinter::write_values(const Eigen::Vector2d& value) {
+    std::ofstream file(filename, std::ios::app);
+    if (!file.good()) {
+        return;
+    }  // add error throwing
+    file << value(0) << separator << value(1) << std::endl;
 }
 
-datPrinter::datPrinter() {
+datPrinter::datPrinter() : FilePrinter() {
     filename.append(".dat");
 }
 
-void datPrinter::write_values(Eigen::Vector2d value) {
-    std::cout << "x = " << value(0) << "--- f(x) = " << value(1) << std::endl;
+void datPrinter::write_values(const Eigen::Vector2d& value) {
+    std::ofstream file(filename, std::ios::app);
+    if (!file.good()) {
+        return;
+    }  // add error throwing
+    file << value(0) << " " << value(1) << std::endl;
 }
--- ROOT/ROOT/writer.hpp
+++ ROOT/ROOT/writer.hpp
@@ -1,5 +1,7 @@
 #ifndef ROOT_WRITER_HPP
 #define ROOT_WRITER_HPP
+#include <fstream>
+#include <iostream>
 #include <string>
 
 #include "../external/eigen/Eigen/Dense"
@@ -8,34 +10,26 @@ class Printer;
 
 class Writer {
   protected:
-    Eigen::Matrix2d values;
+    Eigen::MatrixX2d values;
 
   public:
-    Writer(Eigen::Matrix2d& vals_to_write);
+    Writer(const Eigen::MatrixX2d& vals_to_write);
     void writing_process();
     void choose_how(std::unique_ptr<Printer>& printer);
-    void print_final_result();
+    void print_final_result() const;
 };
 
 class Printer {
   public:
     Printer() = default;
-    virtual void write_values(Eigen::Vector2d value) = 0;
-};
-
-class VisualPrinter : public Printer {
-  private:
-    std::string filename;
-
-  public:
-    VisualPrinter();
-    void write_values(Eigen::Vector2d value) override;
+    virtual ~Printer() = default;
+    virtual void write_values(const Eigen::Vector2d& value) = 0;
 };
 
 class OutputPrinter : public Printer {
   public:
     OutputPrinter();
-    void write_values(Eigen::Vector2d value) override;
+    void write_values(const Eigen::Vector2d& value) override;
 };
 
 class FilePrinter : public Printer {
@@ -49,7 +43,7 @@ class FilePrinter : public Printer {
 class datPrinter : public FilePrinter {
   public:
     datPrinter();
-    void write_values(Eigen::Vector2d value) override;
+    void write_values(const Eigen::Vector2d& value) override;
 };
 
 class csvPrinter : public FilePrinter {
@@ -58,7 +52,28 @@ class csvPrinter : public FilePrinter {
 
   public:
     csvPrinter();
-    void write_values(Eigen::Vector2d value) override;
+    void write_values(const Eigen::Vector2d& value) override;
+};
+
+// LLM
+class GnuplotPrinter : public Printer {
+  private:
+    std::string filename;
+
+  public:
+    GnuplotPrinter(const std::string& fname) : filename(fname + ".dat") {}
+
+    void write_values(const Eigen::Vector2d& value) override {
+        std::ofstream file(filename, std::ios::app);
+        file << value(0) << " " << value(1) << "\n";
+    }
+
+    void generate_gnuplot_script() const {
+        std::ofstream script("plot.plt");
+        script << "set terminal pngcairo size 800,600\n"
+               << "set output 'plot.png'\n"
+               << "plot '" << filename << "' using 1:2 with linespoints\n";
+    }
 };
 
 #endif  // ROOT_WRITER_HPP
