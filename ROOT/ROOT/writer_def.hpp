c80643dfaa1eecbfd1ebabd51584811866502b60
andreasaporito <andrea.saporito03@gmail.com>
2025-12-08 11:50:24 +0100

    Add comments in writer_def.hpp

--- ROOT/ROOT/writer_def.hpp
+++ ROOT/ROOT/writer_def.hpp
@@ -1,3 +1,15 @@
+/**
+ * @file writer_def.hpp
+ * @brief Contains definitions for classes Writer and Printer to Write on different output destinations
+ *
+ * These classes themselves could be used for other kinds of writing, and do not have any relationship
+ * with the solver process.
+ * The Writer class handles the writing outer process, meanwhile the printers effectively write on the output or
+ * in the given file the results.
+ * The Printer classes actually print a given value in the specified output, getting just one value from the
+ * ones stored in Writer object. They can write on the output, on .csv or .dat files, or they can
+ * write on a .dat file and then create a gnuplot.
+ */
 #ifndef ROOT_WRITER_DEF_HPP
 #define ROOT_WRITER_DEF_HPP
 #include <Eigen/Dense>
@@ -7,44 +19,48 @@
 
 enum WritingMethod { CONSOLE, CSV, DAT, GNUPLOT };
 
-/**
- * Polymorphic Writer and Printer classes for the results of the root finding process.
- * These classes themselves could be used for other kinds of writing, and do not have any relationship
- * with the solver process, except for being constructed there as object.
- * The Writer class handles the writing outer process, meanwhile the printers effectively write on the output or
- * in the given file the results.
- */
-
 template <typename V>
 class PrinterBase;
 
 /**
- * \brief Class which stores the values to write in an argument, constructs the Printer and handles the printing process
+ * @brief Writer class - stores required arguments and handles the printing flow
  */
 template <typename T>
 class Writer {
   protected:
-    T values;
-    WritingMethod method;
+    T values; //!< Templated values to write to allow different usage of the class
+    WritingMethod method; //!< Method to write with - defined thanks to @author Saransh-cpp config
 
   public:
+    /**
+     * @brief Constructor for a Writer object
+     *
+     * @param vals_to_write Values to be written, which will be stored in values argument
+     * @param write_method Method to be used, which will be stored in method argument
+     */
     Writer(const T& vals_to_write, WritingMethod write_method);
-    /** \brief Method to run the printing loop and correctly initialize the Printer*/
+    /** @brief Method to run the printing loop and correctly initialize the Printer
+     *
+     * @param filename Name of the file to write on (optional)
+     * @param sep Separator for .csv extension files (optional)
+     * @param overwrite Option to overwrite or append to the file (optional)
+     */
     void write(std::string filename = "", char sep = ',', bool overwrite = true);
-    /** \brief Converts the generic Printer into a typed one for a specific output destination*/
+    /** @brief Method to convert the generic Printer into a typed one for a specific output destination
+     *
+     * @param printer The original abstract printer
+     * @param filename The file to write on
+     * @param sep The separator for .csv files
+     * @param overwrite Option to overwrite or append the file
+     */
     template <typename V>
     void build_printer(std::unique_ptr<PrinterBase<V>>& printer, std::string filename, char sep = ',',
                        bool overwrite = true);
-    /** \brief Helper just to print the final result of the solver process*/
+    /** @brief Helper just to print the final result of the solver process*/
     void print_final_result() const;
 };
 
-/** The Printer classes actually print a given value in the specified output, getting just one value from the
- * ones stored in Writer object. They can write on the output, on .csv or .dat files, or they can
- * write on a .dat file and then create a gnuplot.
- */
-
-/** \brief Abstract Printer class*/
+/** @brief Abstract Printer class*/
 template <typename V>
 class PrinterBase {
   public:
@@ -53,40 +69,50 @@ class PrinterBase {
     virtual void write_values(const V& value) = 0;
 };
 
-/** \brief The class to print out the values in the output*/
+/** @brief The class to print out the values in the CLI*/
 template <typename V>
 class PrinterCLI : public PrinterBase<V> {
   public:
+    /** @brief Constructor for the PrinterCLI class - just prints out a string*/
     PrinterCLI();
-    /** \brief Method to actually print a given value in the output*/
+    /** @brief Method to actually print a given value in the output
+     *
+     * @param value The given value to be printed
+     */
     void write_values(const V& value) override;
 };
 
-/** \brief Mother class for all the Printers which print in a file*/
+/** @brief Mother class for all the Printers which print in a file*/
 template <typename V>
 class PrinterFile : public PrinterBase<V> {
   protected:
-    /** \brief The name of output file, without the extension*/
-    std::string filename;
-    /** \brief Boolean which defines whether at a new Writing process the output file (if already existent), has to
-     * be overwritten or not
-     */
-    bool append;
-    /** \brief The actual file, saved in order not to access it at each printing iteration*/
-    std::ofstream file;
+    std::string filename; //!< The name of output file, without the extension
+    bool append; //!< Boolean which defines whether at a new Writing process the output file (if already existent), has to be overwritten or not
+    std::ofstream file; //!< The actual file, saved in order not to access it at each printing iteration
 
   public:
-    /** \brief The constructor gets as input the filename string and the boolean for overwriting option*/
+    /** @brief The constructor for PrinterFile class
+     *
+     * @param fname Name of the file to write on
+     * @param ow_mode Option to overwrite or append to the file
+     */
     PrinterFile(const std::string& fname, bool ow_mode);
 };
 
-/** \brief Class to write on .dat the result, from which Gnu Printer will inherit*/
+/** @brief Class to write on .dat the result - daughter of FilePrinter and Mother of GnuPlotPrinter*/
 template <typename V>
 class PrinterDAT : public PrinterFile<V> {
   public:
-    /** @brief Constructor taking the filename and whether to overwrite or not the file */
+    /** @brief Constructor for the PrinterDAT class
+     *
+     * @param fname The name of the file to write on
+     * @param ow_mode Option to overwrite or append the file
+     */
     PrinterDAT(const std::string& fname, bool ow_mode);
-    /** @brief Writes a given result into the .dat file*/
+    /** @brief Writes a given result into the .dat file
+     *
+     * @param value The value to write
+     */
     void write_values(const V& value) override;
 };
 
@@ -94,24 +120,32 @@ class PrinterDAT : public PrinterFile<V> {
 template <typename V>
 class PrinterCSV : public PrinterFile<V> {
   private:
-    /** @brief Separator for the .csv output file, given at construction time */
-    char separator;
+    char separator; //!< Separator for the .csv output file, given at construction time
 
   public:
-    /** Constructor taking the filename, the separator and whether to overwrite or not the file */
+    /** @brief Constructor of the PrinterCSV class
+     *
+     * @param fname The file to write on
+     * @param sep The separator to use
+     * @param ow_mode Option to overwrite or append the file
+     */
     PrinterCSV(const std::string& fname, char sep, bool ow_mode);
-    /** @brief Writes a given result into the .csv file */
+    /** @brief Writes a given result into the .csv file with the sotred separator */
     void write_values(const V& value) override;
 };
 
-/** Daughter of PrinterDAT lass to write on .dat (inherited) and produce a gnu plot for the results*/
+/** @brief Class Daughter of PrinterDAT to write on .dat (inherited) and produce a gnu plot for the results*/
 template <typename V>
 // LLM
 class PrinterGNUPlot : public PrinterDAT<V> {
   public:
-    /** @brief Constructor taking the filename and whether to overwrite the file or not */
+    /** @brief Constructor for the PrinterGNUPlot
+     *
+     * @param fname The file to write on
+     * @param ow_mode Option to overwrite or append the file
+     */
     PrinterGNUPlot(const std::string& fname, bool ow_mode);
-    /** @brief Callsthe generation of the .png output plot */
+    /** @brief Calls the generation of the .png output plot */
     void generate_gnuplot_script() const;
 };
 
