29f577afe0d78c9213e21231b425d5a0cf06e615
andreasaporito <andrea.saporito03@gmail.com>
2025-12-08 15:42:30 +0100

    Add aitken and convergence tests

--- libROOT/tests/test_lib_root.cpp
+++ libROOT/tests/test_lib_root.cpp
@@ -9,6 +9,11 @@
 #include <array>
 #include <sstream>
 
+// TO DO:
+// Right now the tests are just checking whether the root is near its computed respective within a given tolerance,
+// but actually the solver stops even if f(x) is below the tolerance, so these tests are not complete
+// -> BUG TO FIX LATER BY ADDING MORE THINGS TO TEST IN EACH TESTER
+
 //LLM helper to read from CLI
 static std::string exec_command(const std::string& cmd) {
     std::array<char, 256> buffer{};
@@ -290,4 +295,145 @@ TEST(FixedPointMethod, DivergentFP) {
     ASSERT_NE(output.find(expectedError), std::string::npos)
         << "Expected error message not found.\nOutput was:\n"
         << output;
+}
+
+TEST(FixedPointMethod, AitkenSpeed) {
+    std::string exe = "../../../build/ROOT/ROOT/root_cli";
+
+    std::string cmd1 =
+        exe +
+        " --wcli --verbose cli"
+        " --function 'cos(x)'"
+        " fixed_point"
+        " --initial 0.5"
+        " --g-function 'cos(x)'";
+
+    std::string cmd2 =
+        exe +
+        " --wcli --verbose cli"
+        " --function 'cos(x)'"
+        " --aitken"
+        " fixed_point"
+        " --initial 0.5"
+        " --g-function 'cos(x)'";
+
+    std::string output1 = exec_command(cmd1);
+    std::string output2 = exec_command(cmd2);
+
+    ASSERT_FALSE(output1.empty());
+    ASSERT_FALSE(output2.empty());
+
+    const std::string token = "Converged in ";
+    auto pos1 = output1.find(token);
+    auto pos2 = output2.find(token);
+
+    ASSERT_NE(pos1, std::string::npos)
+        << "Output did not contain expected root line.\nOutput was:\n"
+        << output1;
+
+    ASSERT_NE(pos2, std::string::npos)
+        << "Output did not contain expected root line.\nOutput was:\n"
+        << output2;
+
+    pos1 += token.size();
+    pos2 += token.size();
+
+    // Extract the number after the token
+    std::stringstream ss1(output1.substr(pos1));
+    int iter1 = 0;
+    ss1 >> iter1;
+
+    std::stringstream ss2(output2.substr(pos2));
+    int iter2 = 0;
+    ss2 >> iter2;
+
+    EXPECT_TRUE(iter2 < iter1);
+}
+
+TEST(FixedPointMethod, ToleranceEffectiveness) {
+    std::string exe = "../../../build/ROOT/ROOT/root_cli";
+
+    std::string cmd1 =
+        exe +
+        " --wcli --verbose cli"
+        " --function 'cos(x)'"
+        " --tolerance 0.001"
+        " fixed_point"
+        " --initial 0.5"
+        " --g-function 'cos(x)'";
+
+    std::string cmd2 =
+        exe +
+        " --wcli --verbose cli"
+        " --function 'cos(x)'"
+        " --tolerance 0.01"
+        " fixed_point"
+        " --initial 0.5"
+        " --g-function 'cos(x)'";
+
+    std::string output1 = exec_command(cmd1);
+    std::string output2 = exec_command(cmd2);
+
+    ASSERT_FALSE(output1.empty());
+    ASSERT_FALSE(output2.empty());
+
+    const std::string token = "Converged in ";
+    auto pos1 = output1.find(token);
+    auto pos2 = output2.find(token);
+
+    ASSERT_NE(pos1, std::string::npos)
+        << "Output did not contain expected root line.\nOutput was:\n"
+        << output1;
+
+    ASSERT_NE(pos2, std::string::npos)
+        << "Output did not contain expected root line.\nOutput was:\n"
+        << output2;
+
+    pos1 += token.size();
+    pos2 += token.size();
+
+    // Extract the number after the token
+    std::stringstream ss1(output1.substr(pos1));
+    int iter1 = 0;
+    ss1 >> iter1;
+
+    std::stringstream ss2(output2.substr(pos2));
+    int iter2 = 0;
+    ss2 >> iter2;
+
+    EXPECT_TRUE(iter2 < iter1);
+}
+
+TEST(NewtonMethod, SlowConvergence) {
+    std::string exe = "../../../build/ROOT/ROOT/root_cli";
+
+    std::string cmd =
+        exe +
+        " --wcli cli"
+        " --function x^2"
+        " newton"
+        " --initial -15"
+        " --derivative 2*x";
+
+    std::string output = exec_command(cmd);
+
+    ASSERT_FALSE(output.empty());
+
+    // Look for the line: "The found root is 0"
+    const std::string token = "The found root is ";
+    auto pos = output.find(token);
+
+    ASSERT_NE(pos, std::string::npos)
+        << "Output did not contain expected root line.\n"
+        <<  "Output was:\n"
+        << output;
+
+    pos += token.size();
+
+    std::stringstream ss(output.substr(pos));
+    double root = 0.0;
+    ss >> root;
+
+    // Validate the root value
+    EXPECT_NEAR(root, 0.0, 1e-2);
 }
